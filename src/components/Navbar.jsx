41e7c5f13e23dc916bb19e6e33a703173452bd8c
anupkr11 <anup94622@gmail.com>
2026-02-17 11:44:47 +0530

    updating data into redux store

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Navbar = () => {
+  const user = useSelector((store)=>store.user)
   return (
     <div className="navbar bg-base-300 shadow-sm">
         <div className="flex-1">
           <a className="btn btn-ghost text-xl">devConnect</a>
         </div>
-        <div className="flex gap-2">
+        {user && <div className="flex gap-2">
+          <div className='flex items-center'>Welcome {user.firstName}</div>
           <div className="dropdown dropdown-end mx-5">
             <div
               tabIndex={0}
@@ -38,7 +41,7 @@ const Navbar = () => {
               </li>
             </ul>
           </div>
-        </div>
+        </div>}
       </div>
   )
 }
